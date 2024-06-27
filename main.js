e371153a08713327d01171cc3b2b496dc1e4ee20
Aziza0011 <khakimovaaziza95@gmail.com>
2024-06-27 11:17:38 +0500

    sssss

---
+++ main.js
@@ -0,0 +1,9 @@
+let username = prompt("Enter here your name");
+let ID = +prompt("Enter here your ID");
+let password = +prompt("Enter here your password");
+let data = {
+  name: username,
+  ID: ID,
+  password: password,
+};
+console.log(data);
